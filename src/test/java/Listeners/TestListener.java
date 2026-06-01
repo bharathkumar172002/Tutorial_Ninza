b1786678d99c679e4a18e4eb3c29aa7eacda6fbf
Bharath kumar <bharathkumar172002@gmail.com>
2026-06-01 23:08:21 +0530

    Initial project commit with updated structure

--- src/test/java/Listeners/TestListener.java
+++ src/test/java/Listeners/TestListener.java
@@ -5,34 +5,54 @@ import org.testng.ITestListener;
 import org.testng.ITestResult;
 
 public class TestListener implements ITestListener {
-    @Override
-    public void onStart(ITestContext context) {
-        System.out.println("Test Execution Started ");
-    }
 
-    @Override
-    public void onTestStart(ITestResult result) {
-        System.out.println("Executing Test : " + result.getName());
-    }
+	@Override
+	public void onStart(ITestContext context) {
 
-    @Override
-    public void onTestSuccess(ITestResult result) {
-        System.out.println("Test Passed : " + result.getName());
-    }
+		System.out.println(
+		"Test Execution Started ");
+	}
 
-    @Override
-    public void onTestFailure(ITestResult result) {
-        System.out.println("Test Failed : " + result.getName());
-        System.out.println("Failure Reason : " + result.getThrowable());
-    }
+	@Override
+	public void onTestStart(ITestResult result) {
 
-    @Override
-    public void onTestSkipped(ITestResult result) {
-        System.out.println("Test Skipped : " + result.getName());
-    }
+		System.out.println(
+		"Executing Test : "
+		+ result.getName());
+	}
 
-    @Override
-    public void onFinish(ITestContext context) {
-        System.out.println("Test Execution Completed ");
-    }
+	@Override
+	public void onTestSuccess(ITestResult result) {
+
+		System.out.println(
+		"Test Passed : "
+		+ result.getName());
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+
+		System.out.println(
+		"Test Failed : "
+		+ result.getName());
+
+		System.out.println(
+		"Failure Reason : "
+		+ result.getThrowable());
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+
+		System.out.println(
+		"Test Skipped : "
+		+ result.getName());
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+
+		System.out.println(
+		"Test Execution Completed ");
+	}
 }
--- src/test/java/StepDefination/EcommerceStep.java
+++ src/test/java/StepDefination/EcommerceStep.java
@@ -1,100 +1,150 @@
-package StepDefinition;
+package StepDefination;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.devtools.v146.page.Page;
 import org.testng.asserts.SoftAssert;
 
 import Hooks.Hook;
-import POM_Test.Page;
+import Pages.EcommercePOM;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.*;
 
-public class Ninja_Cart {
+public class EcommerceStep{
+
     WebDriver driver;
-    Page login;
+
+   
+
     Hook h = new Hook();
-    SoftAssert softAssert = new SoftAssert();
+
+    SoftAssert softAssert =
+    new SoftAssert();
 
     @Before
     public void startBrowser() {
+
         h.setup();
+
         driver = Hook.driver;
     }
 
+    EcommercePOM login;
+
     @Given("login page should be open in default browser")
     public void launch_application() {
-        login = new Page(driver);
+
+        login = new EcommercePOM(driver);
+
         login.openLoginPage();
     }
 
     @When("click on email address field and add valid email {string}")
     public void enter_email(String email) {
-        login.enterEmail("bharathkumar172002@gmail.com");
+
+        login.enterEmail(email);
     }
 
     @And("then click on password field and enter valid {string}")
     public void enter_password(String password) {
-        login.enterPassword("bharath@123");
+
+        login.enterPassword(password);
     }
 
     @And("now click on login button {string}")
     public void click_login(String status) {
-        login.clickLogin("success");
+
+        login.clickLogin(status);
     }
 
     @Then("login successfully and redirect to ninja home page")
     public void verify_dashboard() {
+
         login.verifyDashboard();
     }
 
     @When("ninja search input field receives {string}")
-    public void search_product(String product) throws InterruptedException {
-        WebElement searchBox = driver.findElement(By.name("search"));
+    public void search_product(String product)
+    throws InterruptedException {
+
+        WebElement searchBox =
+        driver.findElement(By.name("search"));
+
         searchBox.clear();
+
         searchBox.sendKeys(product);
-        driver.findElement(By.cssSelector("button.btn-default.btn-lg")).click();
+
+        driver.findElement(
+        By.cssSelector("button.btn-default.btn-lg"))
+        .click();
+
         Thread.sleep(2000);
     }
 
     @Then("ninja custom product list matches criteria")
     public void verify_search_result() {
-        System.out.println("Relevant products displayed successfully.");
+
+        System.out.println(
+        "Relevant products displayed successfully.");
     }
 
     @When("user clicks on add to cart button for the item")
-    public void add_product_to_cart() throws InterruptedException {
-        WebElement addCartButton = driver.findElement(By.xpath("(//button[contains(@onclick,'cart.add')])[1]"));
+    public void add_product_to_cart()
+    throws InterruptedException {
+
+        WebElement addCartButton =
+        driver.findElement(
+        By.xpath("(//button[contains(@onclick,'cart.add')])[1]"));
+
         addCartButton.click();
+
         Thread.sleep(2000);
     }
 
     @Then("product should be added to cart successfully")
     public void verify_cart_message() {
-        System.out.println("Cart confirmation message validated.");
+
+        System.out.println(
+        "Cart confirmation message validated.");
     }
 
     @When("user clicks on the black shopping cart button")
-    public void open_cart_page() throws InterruptedException {
-        driver.findElement(By.id("cart-total")).click();
+    public void open_cart_page()
+    throws InterruptedException {
+
+        driver.findElement(
+        By.id("cart-total"))
+        .click();
+
         Thread.sleep(1000);
-        driver.findElement(By.linkText("View Cart")).click();
+
+        driver.findElement(
+        By.linkText("View Cart"))
+        .click();
     }
 
     @And("clicks on the checkout option")
     public void click_checkout_option() {
-        driver.findElement(By.linkText("Checkout")).click();
+
+        driver.findElement(
+        By.linkText("Checkout"))
+        .click();
     }
 
     @Then("user should be redirected to the checkout page")
     public void verify_checkout_page() {
-        System.out.println("Checkout workflow initiated successfully.");
+
+        System.out.println(
+        "Checkout workflow initiated successfully.");
+
         softAssert.assertAll();
     }
 
     @After
     public void close_browser() {
+
         h.closes();
     }
 }
